62c7b16c4c2af12b276926267221cf89ec06d501
Valerie GONGORA <vgongora@c5r2s11.wethinkcode.co.za>
2020-01-14 17:19:07 +0200

    Functions

--- ex17.py
+++ ex17.py
@@ -5,4 +5,26 @@ script, from_file, to_file = argv
 
 print(f"Copying from {from_file} to {to_file}")
 
+in_file = open(from_file)
+indata = in_file.read()
 
+# indata = open(from_file).read()
+
+print(f"The input file is {len(indata)} bytes long")
+
+print(f"Does the output file exist? {exists(to_file)}")
+print("RETURN to cintinue, Ctrl-C to abort")
+input()
+
+# make the file writable as you open it
+# outfile = open(to_file, 'w').write(indata)
+#   ---python automatically closes it
+out_file = open(to_file, 'w')
+out_file.write(indata)
+
+print("All done")
+
+out_file.close()
+in_file.close()
+
+## copy practically overwrites everything
