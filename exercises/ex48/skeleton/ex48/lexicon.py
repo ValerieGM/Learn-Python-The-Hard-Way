0d7ffb0f5131c2db6cbda76363a583f7cfb142fb
ValerieGM <vgongora@student.com>
2020-06-06 03:32:30 +0200

    Beginning of User Input

---
+++ exercises/ex48/skeleton/ex48/lexicon.py
@@ -0,0 +1,12 @@
+# Test First Challenge
+
+def scan(direction):
+    # 1) lexicon tuples
+    # 2) split directon (input ::)
+    # 3) analyse split direction
+    # 4) put words in their respective types
+    # 5) return sentence in form of sets of tuples
+    # return a list of tuple pairings
+    return [('direction', 'north'),
+            ('direction', 'south'),
+            ('direction', 'east')]
